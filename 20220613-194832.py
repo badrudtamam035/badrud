ff335ae8096c4e12326dfc4ac8297f0362d25923
badrudtamam035 <107450112+badrudtamam035@users.noreply.github.com>
2022-06-14 09:39:08 +0700

    Add files via upload

---
+++ 20220613-194832.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import time
+ 
+t = time.localtime(time.time())
+localtime = time.asctime(t)
+str = "Current Time:" + time.asctime(t)
+ 
+print(str);tugas uts/pemrograman/bin/python3
+print{"---PROGRAM PENGECEKAN DATA---"}
+hu=float{input("---MASUKKAN PENGECEKAN DATA---") }
+print{"Hasil dari pengecekan data adalah:,suhu"}
+ 
